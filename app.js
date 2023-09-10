3f32eed7aa65de8d8fc1ce8c02b3bdf98be78780
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2023-09-10 17:47:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navbarMenu = document.querySelector(".navbar-menu");
+
+    menuToggle.addEventListener("click", function () {
+        navbarMenu.classList.toggle("active");
+    });
+});
