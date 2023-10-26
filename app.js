498d9c6c936a571409006ded1161492cc8d74a9f
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2023-10-26 20:56:17 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,14 +6,3 @@ document.addEventListener("DOMContentLoaded", function () {
     navbarMenu.classList.toggle("active");
   });
 });
-
-gsap.from(".service-card", {
-  scrollTrigger: {
-    trigger: "#services",
-    start: "top 80%",
-    toggleActions: "play none none none",
-  },
-  scale: 0.5,
-  opacity: 0,
-  stagger: 0.2,
-});
