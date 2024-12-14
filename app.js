5e78b726680b57ce7c36fd1f85028618a3f9fd19
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2024-12-14 20:38:27 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -19,8 +19,67 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+// ---- Preloader ----
+
+const loader = document.getElementById("preloader");
+
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
+
 // -----------Right click disable code ---------
 
-// document.addEventListener("contextmenu", function (e) {
-//   e.preventDefault();
-// });
+document.addEventListener("contextmenu", function (e) {
+  // e.preventDefault();
+});
+
+// ---- Smooth loading ----
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.body.classList.add('loaded');
+});
+
+// Gsap Animation 
+
+window.addEventListener('load', () => {
+  // Animate the navbar
+  // gsap.from("nav", { duration: 1.5, y: -50, opacity: 0, ease: "power2.out" });
+
+  // Animate the song list containers
+  gsap.from(".container", {
+    duration: 1.5,
+    y: 50,
+    opacity: 0,
+    ease: "power2.out",
+    stagger: 0.2, // Stagger animation for multiple containers
+  });
+
+  // Animate individual song items
+  gsap.from(".songItem", {
+    duration: 1.5,
+    x: -50,
+    opacity: 0,
+    ease: "power2.out",
+    stagger: 0.1, // Stagger animation for song items
+    delay: 0.5,
+  });
+
+  // Animate the bottom player controls
+  gsap.from(".bottom", { duration: 1.5, y: 50, opacity: 0, ease: "power2.out", delay: 1 });
+});
+
+gsap.to('.footer-container', {
+  opacity: 1,
+  y: 0,
+  duration: 1.5,
+  ease: 'power3.out',
+  delay: 0.3
+});
+
+gsap.to('.footer-bottom', {
+  opacity: 1,
+  y: 0,
+  duration: 1.2,
+  ease: 'power3.out',
+  delay: 0.5
+});
